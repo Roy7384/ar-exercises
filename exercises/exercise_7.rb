28a66690e45b53099694357ec9553cad0866357b
Roylu7384 <roylu7384@gmail.com>
2022-04-04 17:02:15 -0400

    Add validations

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,19 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Employee
+  validates :first_name, :last_name, :store_id, presence: true
+  validates :hourly_rate, :inclusion => 40..200
+end
+
+class Store
+  validates :name, length: {minimum: 3}, presence: true
+  validates :annual_revenue, numericality: {greater_than_or_equal_to: 0, only_integer: true}
+
+  def must_carry_at_least_one
+    if !(@mens_apparel || @womens_apparel)
+      errors.add(:apparel, "must carry at least one")
+    end
+  end
+
+end
