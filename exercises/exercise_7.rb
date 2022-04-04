d7d6bde1808457f0bd0fb5b9612ac7af7f74ed87
Roylu7384 <roylu7384@gmail.com>
2022-04-04 17:22:54 -0400

    Add custom validations

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -18,6 +18,7 @@ end
 class Store
   validates :name, length: {minimum: 3}, presence: true
   validates :annual_revenue, numericality: {greater_than_or_equal_to: 0, only_integer: true}
+  validate :must_carry_at_least_one
 
   def must_carry_at_least_one
     if !(@mens_apparel || @womens_apparel)
@@ -29,4 +30,7 @@ end
 
 user_store_name = gets.chomp
 
-Store.new(name: user_store_name)
+user_store = Store.create(name: user_store_name)
+
+puts user_store.valid?
+puts user_store.errors.messages
